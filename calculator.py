a254ed688d44ef9e67e04c6fbb00c0b456274bd5
2218069 <amno0334a@naver.com>
2023-10-23 22:21:14 +0900

    Disaster investigation statistics calculator
    재해조사 통계 계산기

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def calculate(death, worker):
+    if worker == 0:
+        return "근로자수가 0이므로 연천인율을 계산할 수 없습니다."
+    else:
+        ratio = (death / worker) * 1000
+        return ratio
+
+while True:
+    death = int(input("사망자수를 입력하세요 (종료하려면 -1 입력): "))
+
+    if death == -1:
+        break
+
+    worker = int(input("근로자수를 입력하세요: "))
+
+    Disaster_rate = calculate_ratio(death, worker)
+
+print(f"연천인율은 {Disaster_rate:.2f}입니다.")
